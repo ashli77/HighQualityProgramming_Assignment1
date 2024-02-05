84821e6280ed3890c3312b7ee4083307a013132a
hp <hp@LAPTOP-P49JC8OB>
2024-02-05 02:42:41 -0500

    February 4

--- HighQualityProgramming_Assignment1/Program.cs
+++ HighQualityProgramming_Assignment1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-class pet {
+class pet
+{
     static void Main()
     {
         Console.WriteLine("Choose a pet you like from below: \n1. Dog\n2. Cat\n3. Elephant");
@@ -9,26 +10,86 @@ class pet {
             int userinput = Convert.ToInt32(Console.ReadLine());
             if (userinput == 1)
                 Console.WriteLine("You have selected Dog as your pet");
-            
+
             if (userinput == 2)
                 Console.WriteLine("You have seleted Cat as your pet");
-            
+
             if (userinput == 3)
                 Console.WriteLine("You have selected Elephant as your pet");
-                       
+
 
         }
         {
             Console.Write("Enter desired name for your buddy :");
             string userInput1 = Console.ReadLine();
-            Console.WriteLine("Hello" + userInput1);
-            Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello " + userInput1);
+           // Console.ReadLine();
+            
 
         }
+        {
+            //Main Menu
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("1. Feed the Pet");
+            Console.WriteLine("2. Let the Pet play");
+            Console.WriteLine("3. Let the Pet rest");
+            Console.WriteLine("4. Check the current status");
+            Console.WriteLine("5. EXIT");
+            Console.WriteLine("\nChoose any option : ");
+           
+
+        }
+        
+        string userinput2 = Console.ReadLine();
+        Console.WriteLine("User input: " + userinput2);
+        while (userinput2 != "5")
+        {
+            
+            int Hunger = 10;
+            int Health = 10;
+            int Happiness = 10;
 
 
+
+            switch (userinput2)
+            {
+                case "1":
+                    Console.WriteLine();
+                    Hunger = Hunger - 1;
+                    Health = Health + 1;
+                    Console.WriteLine("The Food was delicious, my hunger got reduced and health got increased");
+                break;
+
+                case "2":
+                    Console.WriteLine();
+                    Hunger = Hunger + 1;
+                    Happiness = Happiness + 1;
+                break;
+
+                case "3":
+                    Console.WriteLine();
+                    Hunger = Hunger + 1;
+                    Happiness = Happiness + 1;
+                    Health = Health + 1;
+                break;
+
+                case "4":
+                    Console.WriteLine("Health = " + Health);
+                    Console.WriteLine("Hunger = " + Hunger);
+                    Console.WriteLine("Happiness = " + Happiness);
+                break;
+
+                case "5":
+                    return;
+
+            }
+
+
+        }
     }
-    
-    
 
+
+    
+    
 }
