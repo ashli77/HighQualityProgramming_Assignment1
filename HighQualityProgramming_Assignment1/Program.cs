1b0f623d2b35a5673268d373b4d6060751aaf7b1
hp <hp@LAPTOP-P49JC8OB>
2024-02-06 00:15:58 -0500

    latest edited 06/02

--- HighQualityProgramming_Assignment1/Program.cs
+++ HighQualityProgramming_Assignment1/Program.cs
@@ -24,72 +24,97 @@ class pet
             string userInput1 = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("Hello " + userInput1);
-           // Console.ReadLine();
-            
+            // Console.ReadLine();
+
 
         }
         {
             //Main Menu
-            Console.WriteLine("MAIN MENU");
-            Console.WriteLine("1. Feed the Pet");
-            Console.WriteLine("2. Let the Pet play");
-            Console.WriteLine("3. Let the Pet rest");
-            Console.WriteLine("4. Check the current status");
-            Console.WriteLine("5. EXIT");
-            Console.WriteLine("\nChoose any option : ");
-           
+            //Console.WriteLine("MAIN MENU");
+            //Console.WriteLine("1. Feed the Pet");
+            //Console.WriteLine("2. Let the Pet play");
+            //Console.WriteLine("3. Let the Pet rest");
+            //Console.WriteLine("4. Check the current status");
+            //Console.WriteLine("5. EXIT");
+            //Console.WriteLine("\nChoose any option : ");
 
-        }
-        
-        string userinput2 = Console.ReadLine();
-        Console.WriteLine("User input: " + userinput2);
-        while (userinput2 != "5")
-        {
-            
             int Hunger = 10;
             int Health = 10;
             int Happiness = 10;
 
 
 
-            switch (userinput2)
+            //int userinput2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("User input: " + userinput2);
+            bool loop = true;
+            while (loop)
             {
-                case "1":
-                    Console.WriteLine();
-                    Hunger = Hunger - 1;
-                    Health = Health + 1;
-                    Console.WriteLine("The Food was delicious, my hunger got reduced and health got increased");
-                break;
 
-                case "2":
-                    Console.WriteLine();
-                    Hunger = Hunger + 1;
-                    Happiness = Happiness + 1;
-                break;
+                
+                Console.WriteLine("\nMAIN MENU");
+                Console.WriteLine("1. Feed the Pet");
+                Console.WriteLine("2. Let the Pet play");
+                Console.WriteLine("3. Let the Pet rest");
+                Console.WriteLine("4. Check the current status");
+                Console.WriteLine("5. EXIT");
+                Console.WriteLine("\nChoose any option : ");
 
-                case "3":
-                    Console.WriteLine();
-                    Hunger = Hunger + 1;
-                    Happiness = Happiness + 1;
-                    Health = Health + 1;
-                break;
+                int userinput2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("User input: " + userinput2);
 
-                case "4":
-                    Console.WriteLine("Health = " + Health);
-                    Console.WriteLine("Hunger = " + Hunger);
-                    Console.WriteLine("Happiness = " + Happiness);
-                break;
+                switch (userinput2)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        Hunger = Hunger - 1;
+                        Health = Health + 1;
+                        Console.WriteLine("The Food was delicious, my hunger got reduced and health got increased");
+                        break;
 
-                case "5":
-                    return;
+
+                    case 2:
+                        Console.WriteLine();
+                        Hunger = Hunger + 1;
+                        Happiness = Happiness + 1;
+                        break;
+
+                    case 3:
+                        Console.WriteLine();
+                        Hunger = Hunger + 1;
+                        Happiness = Happiness + 1;
+                        Health = Health + 1;
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nHealth = " + Health);
+                        Console.WriteLine("Hunger = " + Hunger);
+                        Console.WriteLine("Happiness = " + Happiness);
+                        break;
+                    case 5:
+                        Console.WriteLine("bye");
+                        loop = false;
+                        break;
+
+                   default:
+                        Console.WriteLine("emter a valid chioce: ");
+
+                        break;
+                      
+                }
+
+
+
 
             }
-
-
         }
+
+
+
     }
-
-
-    
-    
 }
+
+
+    
+    
+
+
