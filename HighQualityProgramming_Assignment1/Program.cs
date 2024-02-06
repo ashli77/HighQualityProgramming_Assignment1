f47af979b10f6658f1f018777ebde1113e891a83
hp <hp@LAPTOP-P49JC8OB>
2024-02-06 02:57:09 -0500

    06/02_edited

--- HighQualityProgramming_Assignment1/Program.cs
+++ HighQualityProgramming_Assignment1/Program.cs
@@ -4,10 +4,10 @@ class pet
 {
     static void Main()
     {
-        Console.WriteLine("Choose a pet you like from below: \n1. Dog\n2. Cat\n3. Elephant");
-        Console.WriteLine("Choose the number corresponding to the Pet to choose your buddy");
+        Console.WriteLine("Choose a pet you like from below: \n1. Dog\n2. Cat\n3. Elephant"); //Choose the a pet you like
+        Console.WriteLine("Choose the number corresponding to the Pet to choose your buddy");//Each pet is represented corresponding to a number as identity
         {
-            int userinput = Convert.ToInt32(Console.ReadLine());
+            int userinput = Convert.ToInt32(Console.ReadLine());//selection of pet corresponding to the number
             if (userinput == 1)
                 Console.WriteLine("You have selected Dog as your pet");
 
@@ -17,47 +17,38 @@ class pet
             if (userinput == 3)
                 Console.WriteLine("You have selected Elephant as your pet");
 
+            if (userinput >= 3)
+                Console.WriteLine("Choose a valid number");
+
 
         }
         {
-            Console.Write("Enter desired name for your buddy :");
-            string userInput1 = Console.ReadLine();
+            Console.Write("Enter desired name for your buddy :");//choose a name for your pet
+            string userinput1 = Console.ReadLine();
             Console.WriteLine();
-            Console.WriteLine("Hello " + userInput1);
+            Console.WriteLine("Hello " + userinput1);
             // Console.ReadLine();
 
 
         }
         {
             //Main Menu
-            //Console.WriteLine("MAIN MENU");
-            //Console.WriteLine("1. Feed the Pet");
-            //Console.WriteLine("2. Let the Pet play");
-            //Console.WriteLine("3. Let the Pet rest");
-            //Console.WriteLine("4. Check the current status");
-            //Console.WriteLine("5. EXIT");
-            //Console.WriteLine("\nChoose any option : ");
 
-            int Hunger = 10;
-            int Health = 10;
-            int Happiness = 10;
+            int Hunger = 5;
+            
+            int Health = 5;
+           
+            int Happiness = 5;
+           
+            
 
 
-
-            //int userinput2 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("User input: " + userinput2);
             bool loop = true;
             while (loop)
             {
 
                 
-                Console.WriteLine("\nMAIN MENU");
-                Console.WriteLine("1. Feed the Pet");
-                Console.WriteLine("2. Let the Pet play");
-                Console.WriteLine("3. Let the Pet rest");
-                Console.WriteLine("4. Check the current status");
-                Console.WriteLine("5. EXIT");
-                Console.WriteLine("\nChoose any option : ");
+                Console.WriteLine("\nMAIN MENU\n1. Feed the Pet\n2. Let the Pet play\n3. Let the Pet rest\n4. Check the current status\n5. EXIT\n\nChoose any option : ");
 
                 int userinput2 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("User input: " + userinput2);
@@ -68,7 +59,11 @@ class pet
                         Console.WriteLine();
                         Hunger = Hunger - 1;
                         Health = Health + 1;
-                        Console.WriteLine("The Food was delicious, my hunger got reduced and health got increased");
+                        Console.WriteLine("The Food was delicious, my hunger got reduced and health got increased.");
+                        if (Hunger >= 8)
+                            Console.WriteLine("Your pet is about die, give some food");
+                        else if (Hunger <= 2)
+                            Console.WriteLine("I am not much Hungry anymore, I want to play");
                         break;
 
 
@@ -76,27 +71,37 @@ class pet
                         Console.WriteLine();
                         Hunger = Hunger + 1;
                         Happiness = Happiness + 1;
+                        Console.WriteLine("It was really good to play, I am very much happy and a bit hungry as well.");
+                        if (Health >= 8)
+                            Console.WriteLine("I am very healthy, Iwant to play");
+                        else if (Health <= 2)
+                            Console.WriteLine("You pet is about to die");
                         break;
 
                     case 3:
                         Console.WriteLine();
                         Hunger = Hunger + 1;
-                        Happiness = Happiness + 1;
+                        Happiness = Happiness - 1;
                         Health = Health + 1;
+                        Console.WriteLine("It was nice to rest for a while, but I want to play.");
+                        if (Happiness <= 3)
+                            Console.WriteLine("Your pet is about to get depressed");
                         break;
 
                     case 4:
+                        Console.WriteLine("This is my current status right now.......");
                         Console.WriteLine("\nHealth = " + Health);
                         Console.WriteLine("Hunger = " + Hunger);
                         Console.WriteLine("Happiness = " + Happiness);
+                        
                         break;
                     case 5:
-                        Console.WriteLine("bye");
+                        Console.WriteLine("Bye, Come back soon........");
                         loop = false;
                         break;
 
                    default:
-                        Console.WriteLine("emter a valid chioce: ");
+                        Console.WriteLine("Emter a valid chioce: ");
 
                         break;
                       
